afe3be36d7a0ff31c54eb00faa118ead09eeb45e
Rafael p <barboza.rafael.p@gmail.com>
2017-05-15 13:59:43 -0500

    Configurando Retrofit

---
+++ app/src/main/java/com/app/master/petagramsocial/Modelo/ConstantesJsonGetRecentMedia.java
@@ -0,0 +1,17 @@
+package com.app.master.petagramsocial.Modelo;
+
+/**
+ * Created by Rafael p on 15/5/2017.
+ */
+
+public class ConstantesJsonGetRecentMedia {
+    public static final String DATA_OBJECT="data";
+    public static final String USUARIO_OBJECT="user";
+    public static final String IMAGEN_OBJECT="images";
+    public static final String IMAGEN_RESOLUCION_OBJECT="standard_resolution";
+    public static final String LIKE_OBJECT="likes";
+    public static final String ID="id";
+    public static final String NOMBRE="full_name";
+    public static final String LIKE="count";
+    public static final String URL_IMAGEN="url";
+}
---
+++ app/src/main/java/com/app/master/petagramsocial/Modelo/DeserializadorInstagramMediaRecent.java
@@ -0,0 +1,48 @@
+package com.app.master.petagramsocial.Modelo;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+/**
+ * Created by Rafael p on 15/5/2017.
+ */
+
+public class DeserializadorInstagramMediaRecent implements JsonDeserializer<ContactoRespuesta> {
+    @Override
+    public ContactoRespuesta deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        Gson gson = new Gson();
+        ContactoRespuesta contactoRespuesta = gson.fromJson(json, ContactoRespuesta.class);
+        JsonArray jsonArrayData = json.getAsJsonObject().getAsJsonArray(ConstantesJsonGetRecentMedia.DATA_OBJECT);
+        return null;
+    }
+
+    public ArrayList<Contacto> deserializadorContacto(JsonArray jsonArray) {
+        ArrayList<Contacto> contactos = new ArrayList<>();
+        for (int i = 0; i < jsonArray.size(); i++) {
+            Contacto contacto = new Contacto();
+            JsonObject jsonObjeto = jsonArray.get(i).getAsJsonObject();
+
+            JsonObject jsonUser = jsonObjeto.getAsJsonObject(ConstantesJsonGetRecentMedia.USUARIO_OBJECT);
+            contacto.setNombre(jsonUser.get(ConstantesJsonGetRecentMedia.NOMBRE).getAsString());
+            contacto.setId(jsonUser.get(ConstantesJsonGetRecentMedia.ID).getAsInt());
+
+
+            JsonObject jsonLike = jsonObjeto.getAsJsonObject(ConstantesJsonGetRecentMedia.LIKE_OBJECT);
+            contacto.setLike(jsonLike.get(ConstantesJsonGetRecentMedia.LIKE).getAsInt());
+
+
+        }
+        return contactos;
+    }
+}
