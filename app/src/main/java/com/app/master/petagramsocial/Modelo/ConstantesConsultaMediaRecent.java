cd218a500cb19d392560737319641a82b7839977
Rafael p <barboza.rafael.p@gmail.com>
2017-05-16 11:18:26 -0500

    Configurando Gson

--- app/src/main/java/com/app/master/petagramsocial/Modelo/ConstantesConsultaMediaRecent.java
+++ app/src/main/java/com/app/master/petagramsocial/Modelo/ConstantesConsultaMediaRecent.java
@@ -5,10 +5,10 @@ package com.app.master.petagramsocial.Modelo;
  */
 
 public class ConstantesConsultaMediaRecent {
-    public final static String URL="https://api.instagram.com/";
-    public final static String VERSION="v1";
+    public final static String URL="https://api.instagram.com";
+    public final static String VERSION="/v1/";
     public final static String URLBASE=URL+VERSION;
-    public final static String CONSULTA="/users/self/media/recent/?access_token=";
+    public final static String CONSULTA="users/self/media/recent/?access_token=";
     public final static String TOKEN="1041594776.a671e79.260048636a4746c88b6382d53576aec1";
     public final static String URLCONSULTA=URLBASE+CONSULTA+TOKEN;
 }
